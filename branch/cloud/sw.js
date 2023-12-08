78ed8e70ef37af9344ad499f1b929682c2b3a3e3
OFFTKP <parisoplop@gmail.com>
2023-12-08 12:22:34 +0000

    Deploying to gh-pages from @ OFFTKP/SkyEmu@2bca5f071c02a65473c86491b9d93062cd43f737 🚀

--- branch/cloud/sw.js
+++ branch/cloud/sw.js
@@ -1,5 +1,5 @@
 // Define the cache version
-const CACHE_VERSION = '2a4946757fef7b9742ec37118726641a1c7c0093';
+const CACHE_VERSION = '2bca5f071c02a65473c86491b9d93062cd43f737';
 
 // Define an array of URLs to cache
 const CACHE_URLS = [
