65a8dc08a2f629abd9148d95addbb474caab80a5
offtkp <parisoplop@gmail.com>
2024-08-15 15:26:56 +0300

    Atlas progress

--- src/atlas.cpp
+++ src/atlas.cpp
@@ -1,12 +1,12 @@
 #include "atlas.h"
 #include "sokol_gfx.h"
 
-#include <array>
 #include <atomic>
 #include <cassert>
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
 #include <mutex>
 #include <string>
 #include <unordered_map>
@@ -32,11 +32,19 @@ struct cached_image_t {
     int height;
 };
 
+struct atlas_tile_info_t {
+    cached_image_t* image_data;
+    float x1, y1, x2, y2; // coordinates in the atlas
+    std::atomic_bool ready = { false }; // whether the tile has been uploaded to the GPU
+};
+
 struct atlas_map_t;
 
 std::mutex cached_images_mutex;
 std::unordered_map<std::string, cached_image_t> cached_images;
-std::atomic<atlas_map_t*> atlas_maps[256] = {};
+
+std::mutex atlas_maps_mutex;
+std::shared_ptr<atlas_map_t> atlas_maps[256] = {};
 
 // Atlases are always square and power of two
 // This always starts as a single tile image, but if a new tile needs to be
@@ -56,9 +64,9 @@ struct atlas_t {
 
     std::mutex mutex;
     std::vector<uint8_t> data; // we construct the atlas here before uploading it to the GPU
-    std::unordered_map<uint32_t, uint32_t> tiles; // maps a tile id to its position in the atlas
-                                                  // this position can change if the atlas is resized
-    std::vector<cached_image_t*> images; // images that are already added to the atlas
+
+    // Maps a tile id to its image data and its position in the atlas, which can change
+    std::unordered_map<uint32_t, atlas_tile_info_t> tiles; 
     sg_image image = {};
     uint32_t pixel_stride = 0;
     uint16_t offset_x = 0, offset_y = 0;
@@ -111,16 +119,15 @@ private:
 };
 
 struct atlas_map_t {
-    TODO, use shared_ptr everywhere to make it fully thread safe?
-    std::unordered_map<uint32_t, atlas_t*> atlases;
+    std::mutex atlases_mutex;
+    std::unordered_map<uint32_t, std::shared_ptr<atlas_t>> atlases;
 };
 
 atlas_map_id_t atlas_create_map() {
-    atlas_map_t* map = new atlas_map_t();
+    std::unique_lock<std::mutex> lock(atlas_maps_mutex);
     for (int i = 0; i < 256; i++) {
-        atlas_map_t* expected = nullptr;
-        bool success = atlas_maps[i].compare_exchange_strong(expected, map);
-        if (success) {
+        if (atlas_maps[i] == nullptr) {
+            atlas_maps[i] = std::make_shared<atlas_map_t>();
             return i;
         }
     }
@@ -129,8 +136,11 @@ atlas_map_id_t atlas_create_map() {
 }
 
 void atlas_destroy_map(atlas_map_id_t id) {
-    atlas_map_t* map = atlas_maps[id].exchange(nullptr);
-    delete map;
+    if (atlas_maps[id] == nullptr) {
+        atlas_error("atlas_destroy_map: map is null\n");
+    }
+
+    atlas_maps[id] = nullptr;
 }
 
 atlas_tile_id_t atlas_add_tile_from_url(atlas_map_t* map, const char* url) {
@@ -138,13 +148,13 @@ atlas_tile_id_t atlas_add_tile_from_url(atlas_map_t* map, const char* url) {
 }
 
 bool atlas_has_tile(atlas_tile_id_t id) {
-    atlas_map_t* map = atlas_maps[id.map_id].load();
-
+    std::shared_ptr<atlas_map_t> map = atlas_maps[id.map_id];
+    
     if (map == nullptr) {
         atlas_error("atlas_has_tile: map is null\n");
     }
 
-    atlas_t* atlas = nullptr;
+    std::shared_ptr<atlas_t> atlas = nullptr;
 
     // Find atlas in atlas map
     {
@@ -165,10 +175,56 @@ bool atlas_has_tile(atlas_tile_id_t id) {
     {
         std::unique_lock<std::mutex> lock(atlas->mutex);
         uint32_t tile_id = id.tile_id[0] | (id.tile_id[1] << 8) | (id.tile_id[2] << 16);
-        return atlas->tiles.find(tile_id) != atlas->tiles.end();
+        auto it = atlas->tiles.find(tile_id);
+        if (it == atlas->tiles.end()) {
+            return false;
+        }
+
+        return it->second.ready;
     }
 }
 
 atlas_tile_t atlas_get_tile(atlas_tile_id_t id) {
+    std::shared_ptr<atlas_map_t> map = atlas_maps[id.map_id];
+    
+    if (map == nullptr) {
+        atlas_error("atlas_has_tile: map is null\n");
+    }
 
+    std::shared_ptr<atlas_t> atlas = nullptr;
+
+    // Find atlas in atlas map
+    {
+        std::unique_lock<std::mutex> lock(map->atlases_mutex);
+
+        auto it = map->atlases.find(id.tile_width << 16 | id.tile_height);
+        if (it == map->atlases.end()) {
+            atlas_error("atlas_has_tile: atlas not found\n");
+        }
+
+        atlas = it->second;
+        if (atlas == nullptr) {
+            atlas_error("atlas_has_tile: atlas is null\n");
+        }
+    }
+
+    // Check for the tile in the atlas
+    {
+        std::unique_lock<std::mutex> lock(atlas->mutex);
+        uint32_t tile_id = id.tile_id[0] | (id.tile_id[1] << 8) | (id.tile_id[2] << 16);
+        auto it = atlas->tiles.find(tile_id);
+        if (it == atlas->tiles.end()) {
+            atlas_error("atlas_get_tile: tile not found\n");
+        }
+
+        atlas_tile_info_t& tile_info = it->second;
+        atlas_tile_t tile = {};
+        tile.atlas_id = atlas->image.id;
+        tile.x1 = tile_info.x1;
+        tile.y1 = tile_info.y1;
+        tile.x2 = tile_info.x2;
+        tile.y2 = tile_info.y2;
+
+        return tile;
+    }
 }
