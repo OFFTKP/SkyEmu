b976987199a3bc8c3f7eee67ac5f7214394b2535
offtkp <parisoplop@gmail.com>
2024-08-15 18:53:39 +0300

    More atlas shenanigans

--- src/atlas.cpp
+++ src/atlas.cpp
@@ -1,230 +1,211 @@
-#include "atlas.h"
 #include "sokol_gfx.h"
-
-#include <atomic>
-#include <cassert>
-#include <cstdarg>
-#include <cstdio>
-#include <cstdlib>
-#include <memory>
+#include "atlas.h"
 #include <mutex>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-static_assert(sizeof(atlas_tile_id_t) == 8, "atlas_tile_id_t must be 8 bytes");
-static_assert(sizeof(atlas_map_id_t) == 1, "atlas_map_id_t must be 1 byte"); 
-static_assert(sizeof(atlas_tile_id_t::tile_id) == 3, "atlas_tile_id_t::tile_id must be 3 bytes");
-
-[[noreturn]] void atlas_error(const char* msg, ...) {
-    va_list args;
-    va_start(args, msg);
-    vprintf(msg, args);
-    va_end(args);
+[[noreturn]] void atlas_error(const char* message) {
+    fprintf(stderr, "Atlas error: %s\n", message);
     exit(1);
 }
 
-const int atlas_spacing = 4; // leaving some space between tiles to avoid bleeding
-
 struct cached_image_t {
-    uint8_t* data; // always RGBA
-    int width;
-    int height;
+    const uint8_t* data;
+    uint32_t width, height;
 };
 
-struct atlas_tile_info_t {
-    cached_image_t* image_data;
-    float x1, y1, x2, y2; // coordinates in the atlas
-    std::atomic_bool ready = { false }; // whether the tile has been uploaded to the GPU
-};
+std::mutex image_cache_mutex;
+std::unordered_map<std::string, cached_image_t> image_cache;
 
-struct atlas_map_t;
-
-std::mutex cached_images_mutex;
-std::unordered_map<std::string, cached_image_t> cached_images;
-
-std::mutex atlas_maps_mutex;
-std::shared_ptr<atlas_map_t> atlas_maps[256] = {};
-
-// Atlases are always square and power of two
-// This always starts as a single tile image, but if a new tile needs to be
-// added, it's resized to the next power of two
 struct atlas_t {
-    atlas_t(uint16_t tile_width, uint16_t tile_height)
-        : tile_width(tile_width), tile_height(tile_height)
-    {
-        image.id = SG_INVALID_ID;
-    }
+    atlas_t(uint32_t tile_width, uint32_t tile_height);
+    ~atlas_t();
 
-    ~atlas_t() = default;
-    atlas_t(const atlas_t&) = delete;
-    atlas_t& operator=(const atlas_t&) = delete;
-    atlas_t(atlas_t&&) = delete;
-    atlas_t& operator=(atlas_t&&) = delete;
-
-    std::mutex mutex;
-    std::vector<uint8_t> data; // we construct the atlas here before uploading it to the GPU
-
-    // Maps a tile id to its image data and its position in the atlas, which can change
-    std::unordered_map<uint32_t, atlas_tile_info_t> tiles; 
-    sg_image image = {};
-    uint32_t pixel_stride = 0;
-    uint16_t offset_x = 0, offset_y = 0;
-    const uint16_t tile_width, tile_height;
-    bool resized = false;
-    bool dirty = false; // needs the data to be reuploaded to the GPU
-
-    void add_image(cached_image_t* image)
-    {
-        std::unique_lock<std::mutex> lock(mutex);
-        dirty = true;
-
-        // First, check if we need to resize the atlas
-        // TODO:
-
-
-        uint16_t new_tile_offset_x = offset_x;
-        uint16_t new_tile_offset_y = offset_y;
-
-        offset_x += tile_width + atlas_spacing;
-        if (offset_x + tile_width > pixel_stride)
-        {
-            offset_x = 0;
-            offset_y += tile_width + atlas_spacing;
-        }
-
-        assert(image->width == tile_width);
-
-        for (int y = 0; y < tile_height; y++)
-        {
-            for (int x = 0; x < tile_width; x++)
-            {
-                uint32_t atlas_index =
-                    ((new_tile_offset_x + x) * 4) + (((new_tile_offset_y + y) * pixel_stride) * 4);
-                uint32_t tile_index = x * 4 + (y * 4 * tile_width);
-
-                assert(atlas_index + 3 < data.size());
-                assert(tile_index + 3 < tile_width * tile_height * 4);
-
-                data[atlas_index + 0] = image->data[tile_index + 0];
-                data[atlas_index + 1] = image->data[tile_index + 1];
-                data[atlas_index + 2] = image->data[tile_index + 2];
-                data[atlas_index + 3] = image->data[tile_index + 3];
-            }
-        }
-    }
+    atlas_tile_t* add_image(cached_image_t* image);
+    void upload();
 
 private:
-    void resize();
+    void copy_to_data(cached_image_t* image);
+
+    const uint32_t tile_width, tile_height;
+
+    std::mutex mutex;
+    sg_image image;
+    uint32_t atlas_dimension;
+    uint32_t offset_x, offset_y;
+    std::vector<uint8_t> data;
+    std::unordered_map<cached_image_t*, atlas_tile_t*> images;
+    std::vector<cached_image_t*> images_to_add;
+    bool dirty;   // new data needs to be uploaded to the GPU
+    bool resized; // atlas needs to be destroyed and created at new size
+
+    constexpr static uint32_t padding = 4;
 };
 
 struct atlas_map_t {
+    atlas_tile_t* add_image_from_url(const char* url);
+    atlas_tile_t* add_image_from_path(const char* path);
+
+private:
     std::mutex atlases_mutex;
-    std::unordered_map<uint32_t, std::shared_ptr<atlas_t>> atlases;
+    std::vector<atlas_t*> atlases;
 };
 
-atlas_map_id_t atlas_create_map() {
-    std::unique_lock<std::mutex> lock(atlas_maps_mutex);
-    for (int i = 0; i < 256; i++) {
-        if (atlas_maps[i] == nullptr) {
-            atlas_maps[i] = std::make_shared<atlas_map_t>();
-            return i;
+atlas_t::atlas_t(uint32_t tile_width, uint32_t tile_height) : tile_width(tile_width), tile_height(tile_height) {
+    image.id = SG_INVALID_ID;
+    offset_x = 0;
+    offset_y = 0;
+    dirty = false;
+    resized = false;
+    atlas_dimension = 16;
+    uint32_t minimum_width = tile_width + padding;
+    uint32_t minimum_height = tile_height + padding;
+    while (atlas_dimension < minimum_width || atlas_dimension < minimum_height) {
+        atlas_dimension *= 2;
+    }
+
+    data.resize(atlas_dimension * atlas_dimension * 4);
+}
+
+void atlas_t::copy_to_data(cached_image_t* cached_image) {
+    if (cached_image->width != tile_width || cached_image->height != tile_height) {
+        atlas_error("Image dimensions do not match atlas tile dimensions");
+    }
+
+    dirty = true;
+
+    uint32_t tile_offset_x = offset_x;
+    uint32_t tile_offset_y = offset_y;
+
+    offset_x += tile_width + padding;
+    if (offset_x + tile_width > atlas_dimension) {
+        offset_x = 0;
+        offset_y += tile_height + padding;
+
+        if (offset_y + tile_height > atlas_dimension) {
+            atlas_error("Atlas is full somehow");
         }
     }
 
-    atlas_error("atlas_create_map: too many maps\n");
-}
+    for (int y = 0; y < tile_height; y++) {
+        for (int x = 0; x < tile_width; x++) {
+            uint32_t atlas_offset = ((tile_offset_x + x) * 4) + (((tile_offset_y + y) * atlas_dimension) * 4);
+            uint32_t tile_offset = x * 4 + (y * 4 * tile_width);
 
-void atlas_destroy_map(atlas_map_id_t id) {
-    if (atlas_maps[id] == nullptr) {
-        atlas_error("atlas_destroy_map: map is null\n");
+            data[atlas_offset + 0] = cached_image->data[tile_offset + 0];
+            data[atlas_offset + 1] = cached_image->data[tile_offset + 1];
+            data[atlas_offset + 2] = cached_image->data[tile_offset + 2];
+            data[atlas_offset + 3] = cached_image->data[tile_offset + 3];
+        }
     }
 
-    atlas_maps[id] = nullptr;
+    auto it = images.find(cached_image);
+    if (it == images.end()) {
+        atlas_error("Image not found in atlas");
+    }
+
+    atlas_tile_t* tile = it->second;
+    tile->atlas_id = image.id;
+    tile->x1 = (float)tile_offset_x / atlas_dimension;
+    tile->y1 = (float)tile_offset_y / atlas_dimension;
+    tile->x2 = (float)(tile_offset_x + tile_width) / atlas_dimension;
+    tile->y2 = (float)(tile_offset_y + tile_height) / atlas_dimension;
 }
 
-atlas_tile_id_t atlas_add_tile_from_url(atlas_map_t* map, const char* url) {
+atlas_tile_t* atlas_t::add_image(cached_image_t* image_to_add) {
+    std::unique_lock<std::mutex> lock(mutex);
 
+    // These are the dimensions that would occur after adding a tile
+    uint32_t minimum_x = offset_x + tile_width + padding;
+    uint32_t minimum_y = offset_y + tile_height + padding;
+
+    // If the atlas is too small, resize it
+    if (minimum_x > atlas_dimension || minimum_y > atlas_dimension) {
+        resized = true;
+        atlas_dimension *= 2;
+
+        std::vector<uint8_t> new_data;
+        new_data.resize(atlas_dimension * atlas_dimension * 4);
+        data.swap(new_data);
+
+        offset_x = 0;
+        offset_y = 0;
+
+        for (auto& pair : images) {
+            images_to_add.push_back(pair.first);
+        }
+    }
+
+    float current_x = offset_x;
+    float current_y = offset_y;
+
+    atlas_tile_t* tile = new atlas_tile_t();
+    images[image_to_add] = tile;
+
+    copy_to_data(image_to_add);
+
+    return tile;
 }
 
-bool atlas_has_tile(atlas_tile_id_t id) {
-    std::shared_ptr<atlas_map_t> map = atlas_maps[id.map_id];
+void atlas_t::upload() {
+    std::unique_lock<std::mutex> lock(mutex);
+    if (resized) {
+        sg_destroy_image(image);
+        image.id = SG_INVALID_ID;
+    }
+
+    if (image.id == SG_INVALID_ID) {
+        sg_image_desc desc = {0};
+        desc.type = SG_IMAGETYPE_2D;
+        desc.render_target = false;
+        desc.width = atlas_dimension;
+        desc.height = atlas_dimension;
+        desc.num_slices = 1;
+        desc.num_mipmaps = 1;
+        desc.usage = SG_USAGE_DYNAMIC;
+        desc.pixel_format = SG_PIXELFORMAT_RGBA8;
+        desc.sample_count = 1;
+        desc.min_filter = SG_FILTER_LINEAR;
+        desc.mag_filter = SG_FILTER_LINEAR;
+        desc.wrap_u = SG_WRAP_CLAMP_TO_EDGE;
+        desc.wrap_v = SG_WRAP_CLAMP_TO_EDGE;
+        desc.wrap_w = SG_WRAP_CLAMP_TO_EDGE;
+        desc.border_color = SG_BORDERCOLOR_TRANSPARENT_BLACK;
+        desc.max_anisotropy = 1;
+        desc.min_lod = 0.0f;
+        desc.max_lod = 1e9f;
+
+        image = sg_make_image(desc);
+
+        if (resized) {
+            for (cached_image_t* image_to_add : images_to_add) {
+                copy_to_data(image_to_add);
+            }
+
+            images_to_add.clear();
+            resized = false;
+        }
+    }
+
+    if (dirty) {
+        sg_image_data sg_data = {0};
+        sg_data.subimage[0][0].ptr = data.data();
+        sg_data.subimage[0][0].size = data.size();
+        sg_update_image(image, sg_data);
+        dirty = false;
+    }
+}
+
+atlas_tile_t* atlas_map_t::add_image_from_url(const char* url) {
+    const std::string url_str(url);
     
-    if (map == nullptr) {
-        atlas_error("atlas_has_tile: map is null\n");
-    }
-
-    std::shared_ptr<atlas_t> atlas = nullptr;
-
-    // Find atlas in atlas map
-    {
-        std::unique_lock<std::mutex> lock(map->atlases_mutex);
-
-        auto it = map->atlases.find(id.tile_width << 16 | id.tile_height);
-        if (it == map->atlases.end()) {
-            atlas_error("atlas_has_tile: atlas not found\n");
-        }
-
-        atlas = it->second;
-        if (atlas == nullptr) {
-            atlas_error("atlas_has_tile: atlas is null\n");
-        }
-    }
-
-    // Check for the tile in the atlas
-    {
-        std::unique_lock<std::mutex> lock(atlas->mutex);
-        uint32_t tile_id = id.tile_id[0] | (id.tile_id[1] << 8) | (id.tile_id[2] << 16);
-        auto it = atlas->tiles.find(tile_id);
-        if (it == atlas->tiles.end()) {
-            return false;
-        }
-
-        return it->second.ready;
-    }
 }
 
-atlas_tile_t atlas_get_tile(atlas_tile_id_t id) {
-    std::shared_ptr<atlas_map_t> map = atlas_maps[id.map_id];
-    
-    if (map == nullptr) {
-        atlas_error("atlas_has_tile: map is null\n");
-    }
+atlas_map_t* atlas_create_map() {
+    return new atlas_map_t();
+}
 
-    std::shared_ptr<atlas_t> atlas = nullptr;
-
-    // Find atlas in atlas map
-    {
-        std::unique_lock<std::mutex> lock(map->atlases_mutex);
-
-        auto it = map->atlases.find(id.tile_width << 16 | id.tile_height);
-        if (it == map->atlases.end()) {
-            atlas_error("atlas_has_tile: atlas not found\n");
-        }
-
-        atlas = it->second;
-        if (atlas == nullptr) {
-            atlas_error("atlas_has_tile: atlas is null\n");
-        }
-    }
-
-    // Check for the tile in the atlas
-    {
-        std::unique_lock<std::mutex> lock(atlas->mutex);
-        uint32_t tile_id = id.tile_id[0] | (id.tile_id[1] << 8) | (id.tile_id[2] << 16);
-        auto it = atlas->tiles.find(tile_id);
-        if (it == atlas->tiles.end()) {
-            atlas_error("atlas_get_tile: tile not found\n");
-        }
-
-        atlas_tile_info_t& tile_info = it->second;
-        atlas_tile_t tile = {};
-        tile.atlas_id = atlas->image.id;
-        tile.x1 = tile_info.x1;
-        tile.y1 = tile_info.y1;
-        tile.x2 = tile_info.x2;
-        tile.y2 = tile_info.y2;
-
-        return tile;
-    }
+void atlas_destroy_map(atlas_map_t* map) {
+    delete map;
 }
--- src/atlas.h
+++ src/atlas.h
@@ -11,48 +11,27 @@ extern "C" {
 // A tile in an atlas. atlas_id is the sg_image id of the atlas itself
 // x1, y1, x2, y2 are the coordinates of the tile in the atlas
 typedef struct {
-    uint32_t atlas_id;
+    uint32_t atlas_id = 0;
     float x1, y1;
     float x2, y2;
 } atlas_tile_t;
 
-// There can be up to 256 atlas maps at a time, and each of them can have any number of atlases
-// using different tile sizes
-typedef uint8_t atlas_map_id_t;
+struct atlas_map_t;
 
-// Each atlas tile id is not a pointer, but a descriptor that contains the map id
-// and the necessary info to find a tile in said atlas map
-typedef struct {
-    uint16_t tile_width;
-    uint16_t tile_height;
-    atlas_map_id_t map_id;
-    uint8_t tile_id[3]; // 24-bits of precision, more than enough to represent any number of tiles in our 
-                        // supported tile sizes (width/height >= 8, atlas size <= 4096)
-} atlas_tile_id_t;
+struct atlas_map_t* atlas_create_map();
 
-atlas_map_id_t atlas_create_map();
-
-void atlas_destroy_map(atlas_map_id_t map);
+void atlas_destroy_map(atlas_map_t* map);
 
 // Downloads an image from the url and adds it to the atlas map
-atlas_tile_id_t atlas_add_tile_from_url(atlas_map_id_t map, const char* url);
+// TODO: add hint of atlas total size, so that we can tell it to allocate a bigger atlas or a smaller one if we expect less tiles
+atlas_tile_t* atlas_add_tile_from_url(atlas_map_t* map, const char* url);
 
 // Loads an image from a path and adds it to the atlas map
-atlas_tile_id_t atlas_add_tile_from_path(atlas_map_id_t map, const char* path, bool use_separate_thread);
-
-// Checks if a tile is ready to be used
-bool atlas_has_tile(atlas_tile_id_t id);
-
-// Must be called after atlas_has_tile ensures the tile is ready
-atlas_tile_t atlas_get_tile(atlas_tile_id_t id);
+atlas_tile_t* atlas_add_tile_from_path(atlas_map_t* map, const char* path);
 
 // Called from the main thread at the end of the frame to update the atlases if needed, for example if there's a need to resize
-// or if there are new tiles to add
+// or if there are new tiles to add, or if some atlases need to be cleaned up
 void atlas_upload_all();
-
-// Useful to upload a single atlas map immediately. For example if you add a bunch of images from paths and want to
-// make them immediately available
-void atlas_upload_single(atlas_map_id_t map);
 
 #ifdef __cplusplus
 }
